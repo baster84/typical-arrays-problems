60bde46234e72adbfb9afc7d737393598d1660ba
Alex Borisenkov <baster84@mail.ru>
2020-10-01 01:40:54 +1000

    feat: add min, max and arg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length  == 0) return 0;
+  let min = 0;
+  array.forEach(element => {
+    if (element < min){
+      min = element;
+    }
+  });
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length  == 0) return 0;
+  let max = 0;
+  array.forEach(element => {
+    if (element > max){
+      max = element;
+    }
+  });
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length  == 0) return 0;
+  let sum = 0;
+  array.forEach(element => {
+      sum += element;
+  });
+  return sum / array.length;
 }
